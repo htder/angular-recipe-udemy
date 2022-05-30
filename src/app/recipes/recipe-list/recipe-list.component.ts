30d017c6413dd454a534d1543f3adbff8adc9c28
Henry Derbyshire <henry@htderbyshire.com>
2022-05-30 16:01:32 +0100

    created recipe and ingredient model, started to populate components with business logic

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,7 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://cooksandeats.com/wp-content/uploads/2016/05/IMG_1979.jpg'
+    ),
+    new Recipe(
+      'A Second Test Recipe',
+      'This is a description',
+      'https://cooksandeats.com/wp-content/uploads/2016/05/IMG_1979.jpg'
+    ),
+  ];
 
   constructor() { }
 
