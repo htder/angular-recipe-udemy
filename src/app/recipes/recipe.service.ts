75d75a168ba950f81164e6cbc1f22683664262ce
Henry Derbyshire <henry@htderbyshire.com>
2022-06-06 16:11:20 +0100

    used services to be able to send recipe ingredints to recipe list

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,9 @@
 import {Recipe} from "./recipe.model";
-import {EventEmitter} from "@angular/core";
+import {EventEmitter, Injectable} from "@angular/core";
+import {Ingredient} from "../shared/ingredient.model";
+import {ShoppingListService} from "../shopping-list/shopping-list.service";
 
+@Injectable()
 export class RecipeService {
 
   recipeSelected = new EventEmitter<Recipe>();
@@ -9,17 +12,32 @@ export class RecipeService {
     new Recipe(
       'A Test Recipe',
       'This is simply a test',
-      'https://cooksandeats.com/wp-content/uploads/2016/05/IMG_1979.jpg'
+      'https://cooksandeats.com/wp-content/uploads/2016/05/IMG_1979.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]
     ),
     new Recipe(
       'A Second Test Recipe',
       'This is a description',
-      'https://cooksandeats.com/wp-content/uploads/2016/05/IMG_1979.jpg'
+      'https://cooksandeats.com/wp-content/uploads/2016/05/IMG_1979.jpg',
+      [
+        new Ingredient('Bread', 2),
+        new Ingredient('Meat', 2)
+      ]
     )
   ];
 
+  constructor(private shoppingListService: ShoppingListService) {
+  }
+
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
   }
 
 }
