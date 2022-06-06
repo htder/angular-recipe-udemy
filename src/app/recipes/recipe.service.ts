b08d98e359ecc57448bf71de8d0ec1fc0bbeb3be
Henry Derbyshire <henry@htderbyshire.com>
2022-06-06 15:02:38 +0100

    moved recipes array from recipe list component to recipes service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,3 +1,22 @@
+import {Recipe} from "./recipe.model";
+
 export class RecipeService {
+
+  private recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://cooksandeats.com/wp-content/uploads/2016/05/IMG_1979.jpg'
+    ),
+    new Recipe(
+      'A Second Test Recipe',
+      'This is a description',
+      'https://cooksandeats.com/wp-content/uploads/2016/05/IMG_1979.jpg'
+    )
+  ];
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
 
 }
