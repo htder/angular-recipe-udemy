0204d994d94e9109e744a9d72dae967ab9f44fde
Henry Derbyshire <henry@htderbyshire.com>
2022-06-21 12:15:12 +0100

    connected delete button to onDelete method

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -53,6 +53,12 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     this.editMode = false;
   }
 
+  onDelete() {
+    this.shoppingListService.deleteIngredient(this.editedItemIndex);
+    this.onClear();
+
+  }
+
   ngOnDestroy() {
     this.subscription.unsubscribe();
   }
