4bcbebbaa3188bde4a26fccfb99d68a174007c98
Henry Derbyshire <henry@htderbyshire.com>
2022-06-23 10:25:11 +0100

    added resolver to fetch data when trying to access pages out of order

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -7,26 +7,27 @@ import {Subject} from "rxjs";
 @Injectable()
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
-  private recipes: Recipe[] = [
-    new Recipe(
-      'A Test Recipe',
-      'This is simply a test',
-      'https://cooksandeats.com/wp-content/uploads/2016/05/IMG_1979.jpg',
-      [
-        new Ingredient('Meat', 1),
-        new Ingredient('French Fries', 20)
-      ]
-    ),
-    new Recipe(
-      'A Second Test Recipe',
-      'This is a description',
-      'https://cooksandeats.com/wp-content/uploads/2016/05/IMG_1979.jpg',
-      [
-        new Ingredient('Bread', 2),
-        new Ingredient('Meat', 2)
-      ]
-    )
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'A Test Recipe',
+  //     'This is simply a test',
+  //     'https://cooksandeats.com/wp-content/uploads/2016/05/IMG_1979.jpg',
+  //     [
+  //       new Ingredient('Meat', 1),
+  //       new Ingredient('French Fries', 20)
+  //     ]
+  //   ),
+  //   new Recipe(
+  //     'A Second Test Recipe',
+  //     'This is a description',
+  //     'https://cooksandeats.com/wp-content/uploads/2016/05/IMG_1979.jpg',
+  //     [
+  //       new Ingredient('Bread', 2),
+  //       new Ingredient('Meat', 2)
+  //     ]
+  //   )
+  // ];
+  private recipes: Recipe[] = [];
 
   constructor(private shoppingListService: ShoppingListService) {
   }
