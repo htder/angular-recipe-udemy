8cdad9f9407fdc58aacfe947299f604084da98b4
Henry Derbyshire <henry@htderbyshire.com>
2022-06-24 11:45:32 +0100

    connected sign up form to firebase sign up rest api

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,33 @@
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+
+interface AuthResponseData {
+  kind: string,
+  idToken: string,
+  email: string,
+  refreshToken: string,
+  expiresIn: string,
+  localId: string,
+};
+
+@Injectable({
+  providedIn: "root",
+})
+export class AuthService {
+
+  constructor(
+    private http: HttpClient
+  ) {
+  }
+
+  signUp(email: string, password: string) {
+    return this.http.post<AuthResponseData>(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCGTFnczD-yxgex3Hf6aP8uEXprdtBymnw',
+      {
+        email: email,
+        password: password,
+        returnSecureToken: true,
+      }
+    );
+  }
+}
