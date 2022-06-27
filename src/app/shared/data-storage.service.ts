d5c2ebf5c67b82c84d36264672850a4ef041eb05
Henry Derbyshire <henry@htderbyshire.com>
2022-06-27 11:11:08 +0100

    added an interceptor to attach the user token to

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -27,17 +27,9 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    return this.authService.user.pipe(
-      take(1),
-      exhaustMap(user => {
-        return this.http.get<Recipe[]>(
-          'https://udemy-recipes-597cc-default-rtdb.europe-west1.firebasedatabase.app/recipes.json',
-          {
-            params: new HttpParams().set('auth', user.token)
-          }
-        );
-      }),
-      map(recipes => {
+    return this.http.get<Recipe[]>(
+      'https://udemy-recipes-597cc-default-rtdb.europe-west1.firebasedatabase.app/recipes.json'
+    ).pipe(map(recipes => {
         return recipes.map(recipe => {
           return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
         })
