da85f231340f7f68dba8b23d06bdd86b42182f85
Henry Derbyshire <henry@htderbyshire.com>
2022-06-27 10:09:53 +0100

    added authentication status to user when logged in, which then conditionally renders content

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -2,6 +2,7 @@ import {Component} from "@angular/core";
 import {NgForm} from "@angular/forms";
 import {AuthResponseData, AuthService} from "./auth.service";
 import {Observable} from "rxjs";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-auth',
@@ -13,7 +14,8 @@ export class AuthComponent {
   error: string = null;
 
   constructor(
-    private authService: AuthService
+    private authService: AuthService,
+    private router: Router
   ) {
   }
 
@@ -40,6 +42,7 @@ export class AuthComponent {
     authObs.subscribe(response => {
         console.log(response);
         this.isLoading = false;
+        this.router.navigate(['/recipes']);
       }, errorMessage => {
         this.error = errorMessage;
         this.isLoading = false;
