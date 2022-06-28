3e7ff7e216f10f70b6ab99b54ef8349b82fabf6d
Henry Derbyshire <henry@htderbyshire.com>
2022-06-28 17:10:06 +0100

    added lazy loading to the recipe module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,7 +21,6 @@ import {AuthModule} from "./auth/auth.module";
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    RecipesModule,
     ShoppingListModule,
     SharedModule,
     CoreModule,
