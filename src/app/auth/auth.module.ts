1e6b73d98eef552b3a7555f67fcc372edeab0189
Henry Derbyshire <henry@htderbyshire.com>
2022-06-28 17:13:21 +0100

    added lazy loading to the shopping list and auth module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -11,7 +11,7 @@ import {SharedModule} from "../shared/shared.module";
   imports: [
     SharedModule,
     FormsModule,
-    RouterModule.forChild([{path: 'auth', component: AuthComponent}]),
+    RouterModule.forChild([{path: '', component: AuthComponent}]),
   ]
 })
 export class AuthModule {}
